b725a2624d38ed22667d95d37a2fe7550c56ec89
Jiang <281731686@qq.com>
2019-05-07 16:04:36 +0800

    jcg1 Update Hello.java println

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@
 public class Hello{
 
 	public static void main(String[] args) {
-		System.out.println("jcg 分支进行的第一次修改")
-		System.out.println("GitDemo----1");
+		System.out.println("jcg1修改该文件，并添加了一行打印")
+		System.out.println("================================")
 
 	}
 
